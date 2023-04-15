590f97a7e436b914026b08342bdc50392c409d6a
Matt Thomas <mattcthomas@me.com>
2023-04-15 23:03:36 +0100

    style(prettier): add config

---
+++ .pretterrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config}*/
+const conifg = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
+
+module.exports = conifg;
