dae1d6de0f5693b000569a6ecd8cd08eaa3f88b3
Matt Thomas <mattcthomas@me.com>
2023-04-18 20:25:42 +0100

    chore: delete unused `vscode` folder

--- apps/web/components/ui/page/content/index.tsx
+++ apps/web/components/ui/page/content/index.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 
 type HTMLDivProps = HTMLAttributes<HTMLDivElement>;
 
-export type PageContentProps = PropsWithChildren<HTMLDivProps>
+export type PageContentProps = PropsWithChildren<HTMLDivProps>;
 
 export const Content: FC<PageContentProps> = (props) => {
   return <div {...props} />;
